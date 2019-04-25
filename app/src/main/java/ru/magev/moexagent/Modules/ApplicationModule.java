44a4be38e1d7489a2022be6e40728ab6ee4d3377
mg <morgoth2006@gmail.com>
2019-04-25 17:05:32 +0300

    1.01.4

--- app/src/main/java/ru/magev/moexagent/Modules/ApplicationModule.java
+++ app/src/main/java/ru/magev/moexagent/Modules/ApplicationModule.java
@@ -1,11 +1,35 @@
 package ru.magev.moexagent.Modules;
 
+import android.content.Context;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 import ru.magev.moexagent.Common.StartApplication;
+import ru.magev.moexagent.Interfaces.ApplicationContext;
 
 @Module
 public class ApplicationModule {
 
     StartApplication application;
+
+    public ApplicationModule(StartApplication application) {
+        this.application = application;
+    }
+
+    @Provides
+    @Singleton
+    @ApplicationContext
+    public StartApplication providesApplication() {
+        return application;
+    }
+
+    @Singleton
+    @Provides
+    @ApplicationContext
+    public Context providesContext() {
+        return application.getApplicationContext();
+    }
 
 }
